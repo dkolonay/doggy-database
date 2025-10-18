4881b5c7c444190f4dbd1dbfd2ed37be79a149f7
dkolonay <daniel.kolonay@gmail.com>
2025-10-17 22:22:53 -0400

    provided more semantically relevent classnames for elements in Contact component

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -13,23 +13,23 @@ const Contact = (props) => {
             </div>
 
             <div className={"contact-data-area"}>
-                <h3>{contactData.petName}</h3>
+                <h3 className ={"contact-title"}>{contactData.petName}</h3>
                 <div className={"contact-data-flex-row"}>
                     <div className={"owner-details-block"}>
-                        <h4>Owner Details:</h4>
-                        <p>Name: {contactData.ownerName}</p>
-                        <p>Phone: {contactData.phone}</p>
-                        <p>Email: {contactData.email}</p>
+                        <h4 className={"contact-subtitle"}>Owner Details:</h4>
+                        <p className={"contact-data-point"}>Name: {contactData.ownerName}</p>
+                        <p className={"contact-data-point"}>Phone: {contactData.phone}</p>
+                        <p className={"contact-data-point"}>Email: {contactData.email}</p>
                     </div>
                     <div className={"address-block"}>
-                        <h4>Address:</h4>
-                        <p>
+                        <h4 className={"contact-subtitle"}>Address:</h4>
+                        <p className={"contact-data-point"}>
                             {contactData.address.number} {contactData.address.street}
                         </p>
-                        <p>
+                        <p className={"contact-data-point"}>
                             {contactData.address.city}, {contactData.address.state}
                         </p>
-                        <p>{contactData.address.zip}</p>
+                        <p className={"contact-data-point"}>{contactData.address.zip}</p>
                     </div>
                 </div>
             </div>
